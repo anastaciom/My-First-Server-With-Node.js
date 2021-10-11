b65e32f2c41f0b38d603a10bdc9507af15eedfda
Anastacio Menezes <anastaciomenezes@outlook.com>
2021-10-10 21:36:33 -0300

    My first Server

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url-parse');
+
+http.createServer((req, res) => {
+    let path = new url(req.url)
+    if (path == "" || path == "/") {
+        path = '/index.html'
+    }
+    let fileName = "." + path;
+    
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'content-Type': 'text/html;charset=UTF-8' })
+            res.end('<H1>Page not found</H1>')
+        } else {
+            res.writeHead(200, { 'content-Type': 'text/html; charset=UTF-8' })
+            res.write(data)
+            res.end()
+        }
+    })
+
+}).listen(3000, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Servidor rodando na porta 3000')
+        
+    }
+});
